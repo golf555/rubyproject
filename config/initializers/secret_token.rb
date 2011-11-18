0d93aea2b80f2449e26751e1d62788deb3174aa7
golf555 <golf_lol@mail.ru>
2011-11-19 00:26:09 +0400

    Hello ruby!(first commit)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubyproject::Application.config.secret_token = 'bc71fa0bdeaf82451c2e9a54407becb9b185b9b15f70ce5ead41481e7062217f2baf4bdb4c3d0b46f9428b3921798ef9d6a781fac015e1eefe15e723f75e175b'
